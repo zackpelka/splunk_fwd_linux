9a369cc269c733675653f690e6e89b0870572cb3
Zack Pelka <49172692+zackpelka@users.noreply.github.com>
2020-10-10 14:44:31 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+wget -O splunkforwarder-8.0.6-152fb4b2bb96-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.6&product=universalforwarder&filename=splunkforwarder-8.0.6-152fb4b2bb96-Linux-x86_64.tgz&wget=true'
